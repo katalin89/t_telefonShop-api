3154cc34b0242fac80c3a33e04701b58d4bf62b9
Kati89 <kovacs.katalin@sapientia.ms.ro>
2023-01-12 07:59:29 -1000

    legare prin endpoint

---
+++ src/main/java/ro/mycode/telefonapi/TelefonApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.telefonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ro/mycode/telefonapi/repositpry/TelefonRepo.java
@@ -0,0 +1,2 @@
+package ro.mycode.telefonapi.repositpry;public interface TelefonRepo {
+}
